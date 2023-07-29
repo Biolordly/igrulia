70f47f70eb47bfb9e71e4766ee91e74e227617a0
Biolordly <97603176+Biolordly@users.noreply.github.com>
2023-07-29 11:58:56 +0200

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1690624608,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/camerad-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"images/spawner1-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
